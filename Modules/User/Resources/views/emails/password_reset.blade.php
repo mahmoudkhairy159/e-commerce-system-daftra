dd41a1f70576d7e4a623ab84170a520b69c27a15
mahmoudkhairy159 <mahmoudkhairy159@gmail.com>
2025-06-28 01:06:05 +0300

    fix email credintails problem

--- Modules/User/Resources/views/emails/password_reset.blade.php
+++ Modules/User/Resources/views/emails/password_reset.blade.php
@@ -1,9 +1,6 @@
 @component('mail::message')
 # {{ __('user::app.auth.password_reset.subject') }}
 
-<div style="text-align: center; margin-bottom: 20px;">
-    <img src="{{ $logoUrl }}" alt="{{ $websiteName }}" style="max-width: 150px;">
-</div>
 
 {{ __('user::app.auth.password_reset.intro_line') }}
 
